208565765a70252f5f766bcd286b9a91e59727fd
Johntae Stroud <johntae.stroud@gmail.com>
2025-06-03 23:06:55 -0400

    video update

--- script.js
+++ script.js
@@ -3484,5 +3484,33 @@ function addEventListener_multi(element, eventNames, handler) {
     simp_startScript();
   }
 
-  //
+// YouTube Video
+// LAZY LOADED WITH LAZYSIZES - IN JS
+// https://github.com/aFarkas/lazysizes
 
+
+$("#video-player button").on("click", function () {
+  // Create data attributes for buttons
+  var iframesrc = $(this).attr("data-iframe");
+  var caption = $(this).attr("data-caption");
+  // Target the respective iframe and caption elements in HTML by ID
+  // and update with content of data-attribute in PLAYLIST BUTTON HTML MARK-UP
+  $("#video-player #video-caption").html(caption);
+  $("#video-player iframe").attr("src", iframesrc);
+
+  // Toggle button active class
+  $(this).addClass("active").siblings().removeClass("active");
+
+
+});
+
+// Scroll Playlist horizontally with mousewheel on hover
+// Fork from Álvaro
+// https://codepen.io/alvarotrigo/pen/gOmgRzL
+
+
+const scrollContainer = document.querySelector("#playlist");
+scrollContainer.addEventListener("wheel", evt => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+});
