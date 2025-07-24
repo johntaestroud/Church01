b14c1dcebd9a5dd31262997ca3cb804ed6f0b806
Johntae Stroud <johntae.stroud@gmail.com>
2025-07-24 00:07:39 -0400

    playlist update

--- script.js
+++ script.js
@@ -3764,3 +3764,23 @@ function changePage() {
         }
       });
     });
+
+
+    // Video Playlist Start
+    document.addEventListener("DOMContentLoaded", () => {
+	document.documentElement.style.overflow = "hidden";
+	document.body.style.overflow = "hidden";
+
+	const preview = document.querySelector(".preview");
+
+	if (preview) {
+		if (!window.location.href.includes("fullcpgrid")) {
+			preview.style.display = "none";
+
+			document.documentElement.style.overflow = "";
+			document.body.style.overflow = "";
+		}
+	}
+});
+
+
